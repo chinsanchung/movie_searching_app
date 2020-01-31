9ab647cdbe324a9fff56250b2e99b187cff2c1dd
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-01-31 18:05:43 +0900

    Update MoviePage.js
    1. CSS 수정 : 마진, 배경, 패딩 등
    2. 점수 항목 수정 : 로고 삽입, 해당 사이트로 이동하는 링크 추가, 포스타 하단에서 기존의 위치로 이동

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -3,26 +3,28 @@ import Header from "./Header";
 import { ContainerHome, PosterDiv } from "../style/styledComponents";
 import styled from "styled-components";
 import RottenTomatoes from "../assets/RottenTomatoes";
-import imdbLogo from "../assets/icons8-imdb-48.png";
+import ImdbLogo from "../assets/ImdbLogo";
 
 const SectionPage = styled.div`
     display: flex;
-    padding: 0 50px;
+    padding: 10px 50px;
     box-sizing: border-box;
+    background: #e2e2e2;
     line-height: 1.25;
-    /* &::after {
-        content: "";
-        clear: both;
-    } */
 `;
 const posterSize = {
     width: 350,
     height: 450
 };
 const PageRight = styled.div`
-    /* float: right; */
-    width: 700px;
+    width: 690px;
+    background: #d5d5d5;
+    padding: 0 15px 15px 15px;
+    box-sizing: border-box;
+    border-radius: 5px;
+    margin-left: 5px;
     text-align: left;
+
     .page__title {
         margin-bottom: 0px;
         font-size: 30px;
@@ -30,6 +32,9 @@ const PageRight = styled.div`
     }
     .page__ul {
         margin: 30px 0;
+        padding-bottom: 12px;
+        box-sizing: border-box;
+        border-bottom: 1px solid #cccccc;
     }
     /* page__ul -> detail */
     .detail {
@@ -37,10 +42,10 @@ const PageRight = styled.div`
     }
     .ul--li {
         display: inline-block;
-        padding-right: 5px;
+        padding: 2px 8px 2px 0;
         margin-right: 5px;
         box-sizing: border-box;
-        border-right: 1px solid #e5e5e5;
+        border-right: 1px solid #cccccc;
         font-size: 12px;
     }
     .page--subtitle {
@@ -52,13 +57,18 @@ const PageRight = styled.div`
         color: #666666;
     }
     /* page__ul score */
-    .score {
-        position: absolute;
-        left: 50px;
-        bottom: -55px;
-    }
     .score-li {
-        margin-right: 15px;
+        margin-right: 10px;
+        font-size: 17px;
+    }
+    .meta-score {
+        display: inline-block;
+        background-color: #66cc33;
+        padding: 1px;
+        box-sizing: border-box;
+        text-align: center;
+        vertical-align: 10%;
+        color: #fff;
     }
 `;
 
@@ -73,27 +83,36 @@ function MoviePage(props) {
         Writer,
         Actors,
         Plot,
-        Language,
-        Country,
         Awards,
         Poster,
         Ratings,
         Metascore,
         imdbRating,
-        imdbVotes
+        imdbVotes,
+        imdbID
     } = props.data;
+
+    // Metascore, Rotten Tomatoes 링크 연결을 위해 타이틀 변경
+    const metaTitle = Title.toLowerCase()
+        .split(" ")
+        .join("-");
+    const rottenTitle = Title.toLowerCase()
+        .split(" ")
+        .join("_");
 
     return (
         <>
             <ContainerHome>
                 <Header />
                 <SectionPage>
-                    <PosterDiv
-                        url={Poster}
-                        style={posterSize}
-                        role="movie-poster"
-                        aria-labelledby={Title}
-                    />
+                    <div>
+                        <PosterDiv
+                            url={Poster}
+                            style={posterSize}
+                            role="movie-poster"
+                            aria-labelledby={Title}
+                        />
+                    </div>
                     <PageRight>
                         <div className="page__title">
                             {Title}&nbsp;
@@ -140,44 +159,68 @@ function MoviePage(props) {
                             <p className="page--subtitle">Starring</p>
                             <span>{Actors}</span>
                         </div>
-                        <ul>
-                            <li>{Language}</li>
-                            <li>{Country}</li>
-                            <li>{Awards}</li>
-                        </ul>
+                        <div className="page__ul">
+                            <div>
+                                <p className="page--subtitle">Awards</p>
+                                <span>{Awards}</span>
+                            </div>
+                        </div>
                         <div className="page__ul score">
                             <div className="ul--li score-li">
-                                <RottenTomatoes />
-                                {Ratings.length > 1 &&
-                                Ratings[1].Value !== "N/A"
-                                    ? Ratings[1].Value
-                                    : "     N/A"}
+                                {/* 참고: https://www.thesitewizard.com/html-tutorial/open-links-in-new-window-or-tab.shtml */}
+                                <a
+                                    href={`https://www.rottentomatoes.com/m/${rottenTitle}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <RottenTomatoes />
+                                    &nbsp;
+                                    {Ratings.length > 1 &&
+                                    Ratings[1].Value !== "N/A"
+                                        ? Ratings[1].Value
+                                        : "N/A"}
+                                </a>
                             </div>
                             <div className="ul--li score-li">
-                                <img
-                                    width="55"
-                                    alt="Metacritic logo"
-                                    src="http://bitly.kr/aWJIimdV"
-                                />
-                                &nbsp;
-                                {Metascore !== "N/A" ? Metascore : "   N/A"}
+                                <a
+                                    href={`https://www.metacritic.com/movie/${metaTitle}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <img
+                                        width="50"
+                                        height="15"
+                                        alt="Metacritic logo"
+                                        src="http://bitly.kr/aWJIimdV"
+                                    />
+                                    &nbsp;
+                                    <div className="meta-score">
+                                        {Metascore !== "N/A"
+                                            ? Metascore
+                                            : "N/A"}
+                                    </div>
+                                </a>
                             </div>
-                            <div className="ul--li score-li">
-                                <img
-                                    width="20"
-                                    align="bottom"
-                                    alt="imdb Logo"
-                                    src={imdbLogo}
-                                />
-                                &nbsp;
-                                {imdbRating !== "N/A" ? imdbRating : "   N/A"}
+                            <div
+                                className="ul--li score-li"
+                                style={{ marginRight: 1, borderRight: "none" }}
+                            >
+                                <a
+                                    href={`https://www.imdb.com/title/${imdbID}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <ImdbLogo />
+                                    &nbsp;
+                                    {imdbRating !== "N/A" ? imdbRating : "N/A"}
+                                </a>
                             </div>
                             <div
                                 className="ul--li"
                                 style={{ borderRight: "none" }}
                             >
-                                Votes:{" "}
-                                {imdbVotes !== "N/A" ? imdbVotes : "   N/A"}
+                                ({imdbVotes !== "N/A" ? imdbVotes : "N/A"}
+                                voted)
                             </div>
                         </div>
                     </PageRight>
