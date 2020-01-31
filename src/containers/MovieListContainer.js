13ba0d71f2f010d840cedb69ff0d190af4a51e64
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-01-31 18:06:06 +0900

    Update MovieListContainer.js
    1. 에러, 로딩 페이지 추가

--- src/containers/MovieListContainer.js
+++ src/containers/MovieListContainer.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getMovieListThunk } from "../modules/movie";
 import MovieList from "../components/MovieList";
+import Loading from "../layout/Loading";
+import ErrorPage from "../layout/ErrorPage";
 
 function MovieListContainer({ match }) {
     const { keywords } = match.params;
@@ -14,9 +16,9 @@ function MovieListContainer({ match }) {
         dispatch(getMovieListThunk(keywords));
     }, [keywords, dispatch]);
 
-    if (!data) return null;
-    if (loading && !data) return <div>Loading</div>;
-    if (error) return <p>No Result. Please Input other keywords</p>;
+    if (!data && error) return <ErrorPage />;
+    if (loading && !data) return <Loading />;
+
     return <MovieList movieData={data} />;
 }
 export default MovieListContainer;
