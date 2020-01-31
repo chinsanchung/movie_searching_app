eb4bf5af42988e2f1a42e3dc8b2c238ae537641a
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-01-31 18:09:54 +0900

    Update HomeLayout.js
    1. 공통으로 사용하는 스타일 컴포넌트로 정렬, 디자인
    2. posterStyle 변수 추가
    3. 첫 페이지에 보일 영화 목록을 axios 로 가져와 출력

--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -1,50 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { ListUl, PosterDiv } from "../style/styledComponents";
+import { Link } from "react-router-dom";
+import { getMovieList } from "../api";
 
-const LayoutStyledComponent = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    padding: 30px;
-    box-sizing: border-box;
-`;
-const Image = styled.img`
-    width: 170px;
-    height: 220px;
-    background: no-repeat url("http://bitly.kr/kcqr3Ljn");
-    background-size: cover;
-    margin: 15px;
-    align-content: center;
-    box-sizing: border-box;
-`;
+const LayoutStyledComponent = styled.div``;
+const posterStyle = {
+    width: 170,
+    height: 220,
+    margin: 15
+};
 
 function HomeLayout() {
+    const [list, setList] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getMovieList("out").then(response => {
+            setList(list => list.concat(response.data.Search));
+        });
+        getMovieList("man").then(response => {
+            setList(list => list.concat(response.data.Search));
+            setLoading(false);
+        });
+    }, []);
+
+    if (loading && !list) return <div>LOADING</div>;
+    if (!list) return null;
+
     return (
         <LayoutStyledComponent>
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
-            <Image />
+            <ListUl>
+                {list.map(item => (
+                    <li key={item.imdbID}>
+                        <Link to={`/movie/${item.imdbID}`}>
+                            {item.Poster === "N/A" ? (
+                                <PosterDiv
+                                    url={item.Poster}
+                                    style={{
+                                        ...posterStyle,
+                                        background: "rgb(205, 205, 205)"
+                                    }}
+                                    aria-labelledby={item.Title}
+                                    role="listitem"
+                                />
+                            ) : (
+                                <PosterDiv
+                                    url={item.Poster}
+                                    style={posterStyle}
+                                    aria-labelledby={item.Title}
+                                    role="listitem"
+                                />
+                            )}
+                        </Link>
+                    </li>
+                ))}
+            </ListUl>
         </LayoutStyledComponent>
     );
 }
