d73796a0b59b3e77cd1c4cb3a355eeb364d7c2e3
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-02-06 23:32:55 +0900

    Update HomeLayout.js
    1. posterStyle 객체 삭제
    2. shuffle 함수 추가, shuffle 함수로 바꾼 배열을 상태에 추가
    3. PosterDiv 의 삼항연산자 삭제

--- src/layout/HomeLayout.js
+++ src/layout/HomeLayout.js
@@ -5,23 +5,29 @@ import { Link } from "react-router-dom";
 import { getMovieList } from "../api";
 
 const LayoutStyledComponent = styled.div``;
-const posterStyle = {
-    width: 170,
-    height: 220,
-    margin: 15
-};
 
 function HomeLayout() {
     const [list, setList] = useState([]);
     const [loading, setLoading] = useState(true);
-
+    const shuffle = arr => {
+        // 출처: https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
+    };
     useEffect(() => {
         const getList = async () => {
             try {
                 const res_one = await getMovieList("out");
-                setList(list => list.concat(res_one.Search));
                 const res_two = await getMovieList("man");
-                setList(list => list.concat(res_two.Search));
+
+                const shuffled_list = shuffle([
+                    ...res_one.Search,
+                    ...res_two.Search
+                ]);
+                setList(list => list.concat(shuffled_list));
                 setLoading(false);
             } catch (err) {
                 console.log(err);
@@ -39,24 +45,13 @@ function HomeLayout() {
                 {list.map(item => (
                     <li key={item.imdbID}>
                         <Link to={`/movie/${item.imdbID}`}>
-                            {item.Poster === "N/A" ? (
-                                <PosterDiv
-                                    url={item.Poster}
-                                    style={{
-                                        ...posterStyle,
-                                        background: "rgb(205, 205, 205)"
-                                    }}
-                                    aria-labelledby={item.Title}
-                                    role="listitem"
-                                />
-                            ) : (
-                                <PosterDiv
-                                    url={item.Poster}
-                                    style={posterStyle}
-                                    aria-labelledby={item.Title}
-                                    role="listitem"
-                                />
-                            )}
+                            <PosterDiv
+                                url={item.Poster}
+                                width="170px"
+                                height="220px"
+                                aria-labelledby={item.Title}
+                                role="listitem"
+                            />
                         </Link>
                     </li>
                 ))}
