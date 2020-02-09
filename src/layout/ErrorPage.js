b54313ec8162aa776caf30ca626f470bb7296350
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-02-10 00:04:12 +0900

    Update ErrorPage.js
    1. CSS 수정, NoSign 스타일 컴포넌트 추가
    2. 문구 수정, 이미지 추가

--- src/layout/ErrorPage.js
+++ src/layout/ErrorPage.js
@@ -2,20 +2,33 @@ import React from "react";
 import styled from "styled-components";
 import { ContainerHome } from "../style/styledComponents";
 import Header from "../components/Header";
+import { MdNotInterested } from "react-icons/md";
 
 const ErrorMessage = styled.div`
     margin: 0 auto;
-    padding-top: 150px;
+    padding-top: 40px;
     text-align: center;
     font-size: 30px;
     font-weight: 600;
+`;
+const NoSign = styled.div`
+    margin-top: 150px;
+    font-size: 5em;
+    color: rgb(255, 72, 51);
 `;
 
 function ErrorPage() {
     return (
         <ContainerHome>
             <Header />
-            <ErrorMessage>No Result. Please enter another keyword</ErrorMessage>
+            <div>
+                <NoSign>
+                    <MdNotInterested />
+                </NoSign>
+                <ErrorMessage>
+                    <span>No Result. Please enter another keyword</span>
+                </ErrorMessage>
+            </div>
         </ContainerHome>
     );
 }
