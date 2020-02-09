489d3055728b7028194c0d9c4e55543d199ecb95
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-02-10 00:03:32 +0900

    Update ListLayout.js
    1. 영화 포스터와 북마크의 클릭 이벤트가 겹치지 않도록 Bookmark 컴포넌트를 여기 ListLayout 컴포넌트에 추가

--- src/layout/ListLayout.js
+++ src/layout/ListLayout.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { ListUl } from "../style/styledComponents";
-import Poster from "../components/Poster";
 import { Link } from "react-router-dom";
+import { ListUl, Poster } from "../style/styledComponents";
+import Bookmark from "../components/Bookmark";
 
 function ListLayout({ data }) {
     return (
         <>
             <ListUl>
                 {data.map(data => (
-                    <li key={data.imdbID}>
+                    <li key={data.imdbID} style={{ position: "relative" }}>
+                        <Bookmark
+                            imdbID={data.imdbID}
+                            poster={data.Poster}
+                            title={data.Title}
+                        />
                         <Link to={`/movie/${data.imdbID}`}>
                             <Poster
                                 poster={data.Poster}
-                                title={data.Title}
-                                imdbID={data.imdbID}
                                 width="170px"
                                 height="220px"
                                 aria-labelledby={data.Title}
