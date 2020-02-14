363316450f76f38f911c987f0fbdc7a011a5fbd5
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-02-14 14:35:27 +0900

    Delete js file
    1. js 파일 삭제(ts, tsx 로 변경)

--- src/modules/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import movieReducer from "./movie";
-import bookmarkReducer from "./bookmark";
-
-export default combineReducers({
-    movieReducer,
-    bookmarkReducer
-});
