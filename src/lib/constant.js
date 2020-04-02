fbd6d458368e734fde7497eb78e5d71d339d93e6
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-02 17:29:08 +0900

    create: api 링크 변수, 테스트용 변수들 생성

---
+++ src/lib/constant.js
@@ -0,0 +1,97 @@
+export const API_KEY = "abf55e994773bfc10c3de30ec5debec6";
+export const test_poster =
+    "https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg";
+export const test_backdrop =
+    "https://image.tmdb.org/t/p/original/AbRYlvwAKHs0YuyNO6NX9ofq4l6.jpg";
+const test_poster_path = "/kqjL17yufvn9OVLyXYpvtyrFfak.jpg";
+const test_id = 76341;
+export const test_list = [
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    },
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    },
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    },
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    },
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    },
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    },
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    },
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    },
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    },
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    },
+    {
+        title: "Mad Max: Fury Road",
+        poster_path: `https://image.tmdb.org/t/p/w500${test_poster_path}`,
+        id: test_id
+    }
+];
+
+export const GENRE_LIST = [
+    "Action",
+    "Adventure",
+    "Animation",
+    "Comedy",
+    "Crime",
+    "Documentary",
+    "Drama",
+    "Family",
+    "Fantasy",
+    "History",
+    "Horror",
+    "Music",
+    "Mystery",
+    "Romance",
+    "Science Fiction",
+    "TV Movie",
+    "Thriller",
+    "War",
+    "Western"
+];
+
+const different_genre_between_movie_tv = [
+    "Animation",
+    "Comedy",
+    "Crime",
+    "Documentary",
+    "Drama",
+    "Family",
+    "Mystery",
+    "Western"
+];
