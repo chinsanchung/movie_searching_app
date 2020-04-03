bae5f65db53602535d96da897d37469b5b784a29
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-03 18:40:17 +0900

    update: initialState 에 쓸 함수 작성

---
+++ src/lib/state_util.js
@@ -0,0 +1,24 @@
+const stateUtil = {
+    initial: (data = null) => ({
+        data,
+        loading: false,
+        error: false
+    }),
+    loading: (prevState = null) => ({
+        data: prevState,
+        loading: true,
+        error: false
+    }),
+    success: data => ({
+        data,
+        loading: false,
+        error: false
+    }),
+    error: () => ({
+        data: null,
+        loading: false,
+        error: true
+    })
+};
+
+export default stateUtil;
