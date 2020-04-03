9e3c6c9708e4e8f14bb22908516d2c109f8c09b2
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-03 18:34:25 +0900

    update: Person, Content(Movie, TV Show) 페이지 Route 연결

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Route } from "react-router";
 import MainPage from "./pages/MainPage";
 import "./css/styles.css";
+import ContentDetailContainer from "./containers/ContentDetailContainer";
+import PersonDetailContainer from "./containers/PersonDetailContainer";
 
 function App() {
     return (
         <>
             <Route path="/" exact component={MainPage} />
+            <Route path="/person/:id" component={PersonDetailContainer} />
+            <Route path="/content/:id" component={ContentDetailContainer} />
         </>
     );
 }
