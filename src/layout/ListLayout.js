4c046c02dcdcdfad0385ab32efdf41489d7557eb
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-03 18:36:23 +0900

    update: 데이터 연결, 데이터 종류에 따른 조건문 작성

--- src/layout/ListLayout.js
+++ src/layout/ListLayout.js
@@ -1,24 +1,36 @@
 import React from "react";
-import { test_poster } from "../lib/constant";
-import SimpleBar from "simplebar-react";
+import { Link } from "react-router-dom";
 
 function ListLayout({ list }) {
     return (
-        <div className="p-2">
-            <h4 className="">Action</h4>
-            <div className="row d-flex flex-nowrap list-layout">
-                {list.map(item => (
+        <div className="row d-flex flex-nowrap list-layout">
+            {list.map(item =>
+                item.poster_path ? (
+                    // <Link to={'/content/${item.id}`}>
                     <div className="col-4 col-sm-2 col-md-2 col-lg-2">
                         <div>
                             <img
-                                alt="img"
-                                src={test_poster}
+                                alt="poster_img"
+                                title={item.title}
+                                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
                                 className="img-fluid list-image"
                             />
                         </div>
                     </div>
-                ))}
-            </div>
+                ) : (
+                    // <Link to={'/content/${item.media.id}`}>
+                    <div className="col-4 col-sm-2 col-md-2 col-lg-2">
+                        <div>
+                            <img
+                                alt="related_image"
+                                title={item.media.title}
+                                src={`https://image.tmdb.org/t/p/w500${item.file_path}`}
+                                className="img-fluid list-image"
+                            />
+                        </div>
+                    </div>
+                )
+            )}
         </div>
     );
 }
