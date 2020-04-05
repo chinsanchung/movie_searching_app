f49f2b120d510ec831e4083a8addc54759ff02bd
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-05 23:34:24 +0900

    fix: key 를 item.id 로 변경

--- src/layout/ListLayout.js
+++ src/layout/ListLayout.js
@@ -1,43 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import ListItem from "../components/ListItem";
 
 function ListLayout({ type, list }) {
     return (
         <div className="row d-flex flex-nowrap list-layout">
             {list.map((item, index) => (
-                <ListItem key={index} type={type} item={item} />
+                <ListItem key={item.id} type={type} item={item} />
             ))}
         </div>
     );
 }
 
 export default ListLayout;
-
-/*
-{list.map(item =>
-                item.poster_path ? (
-                    // <Link to={'/content/${item.id}`}>
-                    <div className="col-4 col-sm-2 col-md-2 col-lg-2">
-                        <div>
-                            <img
-                                alt="poster_img"
-                                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                                className="img-fluid list-image"
-                            />
-                        </div>
-                    </div>
-                ) : (
-                    // <Link to={'/content/${item.media.id}`}>
-                    <div className="col-4 col-sm-2 col-md-2 col-lg-2">
-                        <div>
-                            <img
-                                alt="related_image"
-                                src={`https://image.tmdb.org/t/p/w500${item.file_path}`}
-                                className="img-fluid list-image"
-                            />
-                        </div>
-                    </div>
-                )
-            )}
-*/
