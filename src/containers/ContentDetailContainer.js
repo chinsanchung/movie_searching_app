f2795437fa799c7a9e082dbfa1766c593287dc98
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-05 23:32:56 +0900

    update: state 의 loading, 데이터 유무에 따른 조건문 추가

--- src/containers/ContentDetailContainer.js
+++ src/containers/ContentDetailContainer.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import ContentDetail from "../pages/ContentDetail";
 import { useDispatch, useSelector } from "react-redux";
-import { result } from "../lib/madmax";
 import stateUtil from "../lib/state_util";
 import { getMovieDetailThunk } from "../modules/movie";
 
@@ -10,38 +9,22 @@ function ContentDetailContainer(props) {
     const { match } = props;
     const id = match.params.id;
     const { data, loading, error } = useSelector(
-        state => state.movieReducer.content[id] || stateUtil.initial()
+        (state) => state.movieReducer.content[id] || stateUtil.initial()
     );
     useEffect(() => {
         dispatch(getMovieDetailThunk(id));
     }, [dispatch, id]);
     if (!data) return null;
+    if (loading & !data) return null;
+    if (error) return null;
     return (
         <ContentDetail
             detail={data.detail}
             images={data.images}
             credits={data.credits}
-            recommandations={data.recommandations}
             videos={data.videos}
         />
     );
 }
 
 export default ContentDetailContainer;
-
-// data: {
-//     detail: {
-//         // 영화 디테일
-//     },
-//     cast: {
-//         // 영화 캐스트
-//     },
-//     poster: {
-//         포스터
-//     },
-//     images: {
-
-//     },
-//     releated
-
-// }
