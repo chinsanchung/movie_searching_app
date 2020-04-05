fb16e196ae1e4c4f7ed60bb9e0a6cbf860b66c2e
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-05 23:31:38 +0900

    update: Link 추가, poster_path가 null 일 때의 조건문 추가

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 /*
 credit--id, name, profile_path
 images--id X, title X, file_path
@@ -12,18 +12,20 @@ function ListItem({ type, item }) {
     switch (type) {
         case "profile":
             return (
-                <div className="col-4 col-sm-2 col-md-2 col-lg-2">
-                    <div>
+                <div className="col-3 col-sm-2 col-md-2 col-lg-2">
+                    <Link to={`/person/${item.id}`}>
                         <img
-                            alt="poster_img"
+                            alt={item.name}
+                            title={item.name}
                             src={`https://image.tmdb.org/t/p/w500${item.profile_path}`}
                             className="img-fluid list-image"
                         />
-                    </div>
+                    </Link>
                 </div>
             );
         case "file":
             return (
+                //TODO: image file_path는 클릭 시 큰 창이 나오도록
                 <div className="col-4 col-sm-2 col-md-2 col-lg-2">
                     <div>
                         <img
@@ -37,30 +39,27 @@ function ListItem({ type, item }) {
         case "poster":
             return (
                 <div className="col-4 col-sm-2 col-md-2 col-lg-2">
-                    <div>
-                        <img
-                            alt="poster_img"
-                            src={`https://image.tmdb.org/t/p/w500$${item.poster_path}`}
-                            className="img-fluid list-image"
-                        />
-                    </div>
+                    <Link to={`/content/${item.id}`}>
+                        {item.poster_path !== null ? (
+                            <img
+                                alt="poster_img"
+                                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                                className="img-fluid list-image"
+                            />
+                        ) : (
+                            <img
+                                alt="poster_img"
+                                src={""}
+                                className="img-fluid list-image"
+                            />
+                        )}
+                    </Link>
                 </div>
             );
         // TODO: video타입
         default:
             return null;
     }
-    // return (
-    //     <div className="col-4 col-sm-2 col-md-2 col-lg-2">
-    //         <div>
-    //             <img
-    //                 alt="poster_img"
-    //                 src={`https://image.tmdb.org/t/p/w500$`}
-    //                 className="img-fluid list-image"
-    //             />
-    //         </div>
-    //     </div>
-    // );
 }
 
 export default ListItem;
