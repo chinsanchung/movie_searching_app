dbe11866d9645e22c0d1391c6915aabcbf161101
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-05 23:36:44 +0900

    update: 장르별 영화를 검색하는 함수 추가

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { API_KEY } from "./constant";
+import { API_KEY, GENRE_LIST } from "./constant";
 
-const getRequetsedData = async url => {
+const getRequetsedData = async (url) => {
     try {
         const response = await axios.get(url);
         // console.log("api ", response.data);
@@ -11,37 +11,43 @@ const getRequetsedData = async url => {
     }
 };
 // All: Movie + TV Shows
-export const searchAllData = async query => {
+export const searchAllData = async (query) => {
     const url = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${query}&include_adult=false`;
     const response = await getRequetsedData(url);
     return response;
 };
 
-// 출처: https://www.themoviedb.org/talk/59fcec1fc3a368689300071d?language=ko-KR
-// https://api.themoviedb.org/3/discover/movie?api_key=###&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31&vote_average.gte=6&with_genres=28
-export const searchMoviesFromGenre = async genre_id => {
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_average.gte=7&with_genres=${genre_id}`;
-    return url;
+// axios.all: https://www.storyblok.com/tp/how-to-send-multiple-requests-using-axios
+// url 출처: https://www.themoviedb.org/talk/59fcec1fc3a368689300071d?language=ko-KR
+export const searchMoviesFromGenre = async (id) => {
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_average.gte=7&with_genres=${id}`;
+    const response = await getRequetsedData(url);
+    return response;
 };
 
-export const searchPersonDetail = async id => {
+export const searchPersonDetail = async (id) => {
     const filmo_url = `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${API_KEY}`;
     const image_url = ` https://api.themoviedb.org/3/person/${id}/images?api_key=${API_KEY}`;
     return { filmo_url, image_url };
 };
+export const searchMovieRecoommands = async (id) => {
+    const url = `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${API_KEY}&page=1`;
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (err) {
+        console.log(err);
+    }
+};
 
-export const searchMovieDetail = async id => {
+export const searchMovieDetail = async (id) => {
     const detail_url = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
-    // TODO: 메인 크레딧만 나오게 12명정도만 출력하도록
     const credit_url = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}`;
-    const recomm_url = `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${API_KEY}&page=1`;
-    // images: backdrops + posters
+    // const recomm_url = `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${API_KEY}&page=1`;
     const image_url = `https://api.themoviedb.org/3/movie/${id}/images?api_key=${API_KEY}&language=en`;
     const video_url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
-
     const detail_response = await getRequetsedData(detail_url);
     const credit_response = await getRequetsedData(credit_url);
-    const recomm_response = await getRequetsedData(recomm_url);
     const image_response = await getRequetsedData(image_url);
     const video_response = await getRequetsedData(video_url);
 
@@ -49,7 +55,25 @@ export const searchMovieDetail = async id => {
         detail: detail_response,
         images: image_response,
         credits: credit_response.cast,
-        recommendations: recomm_response.results,
-        videos: video_response.results
+        videos: video_response.results,
     };
+    /*
+    const detail_response = axios.get(detail_url);
+    const credit_response = axios.get(credit_url);
+    const image_response = axios.get(image_url);
+    const video_response = axios.get(video_url);
+
+    axios
+        .all([detail_response, credit_response, image_response, video_response])
+        .then(
+            axios.spread((...responses) => {
+                return {
+                    detail: responses[0],
+                    credits: responses[1].cast,
+                    images: responses[2],
+                    videos: responses[3].results,
+                };
+            })
+        )
+        .catch((error) => console.log(error));*/
 };
