701733a3506048f09d5ece495d48d1e217d8a88a
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-06 21:02:53 +0900

    update: state 의 데이터를 React DOM 과 연결, 출력

--- src/pages/PersonDetail.js
+++ src/pages/PersonDetail.js
@@ -1,11 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import ListLayout from "../layout/ListLayout";
-import { test_list } from "../lib/constant";
-import { image_list } from "../lib/bradpittImage";
 
-function PersonDetail({ data }) {
-    const biography = data.biography.split("\n")[0];
+function PersonDetail({ detail, filmography, images, tagged_images }) {
+    const biography = detail.biography.split("\n")[0];
+    const [image_status, setStatus] = useState(true);
+    const [sorted_filmo, setFilmo] = useState(null);
+
+    useEffect(() => {
+        const descFilmo = () => {
+            const filmo_filtered = filmography.filter(
+                (item) => item.release_date !== ""
+            );
+            const result = filmo_filtered.sort(
+                (a, b) =>
+                    parseFloat(b.release_date.split("-")[0]) -
+                    parseFloat(a.release_date.split("-")[0])
+            );
+            setFilmo(result);
+        };
+        descFilmo();
+    }, []);
+
+    if (!sorted_filmo) return null;
 
     return (
         <div>
@@ -16,48 +34,96 @@ function PersonDetail({ data }) {
                 </div>
                 <div className="row">
                     <div className="col-12">
-                        <h3 className="font-weight-bold">{data.name}</h3>
+                        <h3 className="font-weight-bold">{detail.name}</h3>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-12 col-sm-3">
                         <img
-                            alt={data.name}
-                            src={`https://image.tmdb.org/t/p/w500/${data.profile_path}`}
+                            alt={detail.name}
+                            src={`https://image.tmdb.org/t/p/w500/${detail.profile_path}`}
                             className="img-thumbnail mb-2"
                         />
                         <div>
-                            <strong>Born</strong>
-                            <p>{`${data.birthday} in ${data.place_of_birth}`}</p>
+                            <strong>Birthday</strong>
+                            <p>{detail.birthday}</p>
+                        </div>
+                        <div>
+                            <strong>Place of Birth</strong>
+                            <p>{detail.place_of_birth}</p>
                         </div>
                     </div>
                     <div className="col-12 col-sm">
-                        {/* <button
-                            className="btn btn-info d-block d-sm-none"
-                            data-toggle="collapse"
-                            data-target="#collapse-biography"
-                            aria-expanded="false"
-                            aria-controls="collapse-biography"
-                        >
-                            Read biography
-                        </button> */}
-                        <div></div>
                         <div>
                             <h5 className="font-weight-bold pd-4">Biography</h5>
-                            <p>{biography}</p>
+                            <p className="lead">{biography}</p>
+                        </div>
+                        <div>
+                            <div>
+                                <h5 className="font-weight-bold pd-2">
+                                    Filmography
+                                </h5>
+                            </div>
+                            <ul className="list-group">
+                                {sorted_filmo.map(
+                                    (item) =>
+                                        item.release_date !== "" && (
+                                            <li
+                                                key={item.id}
+                                                className="list-group-item"
+                                            >
+                                                <div className="col-2 filmo-text text--year">
+                                                    {
+                                                        item.release_date.split(
+                                                            "-"
+                                                        )[0]
+                                                    }
+                                                </div>
+                                                <Link
+                                                    to={`/content/${item.id}`}
+                                                    className="col-9 filmo-text"
+                                                >
+                                                    {item.title}{" "}
+                                                    <i
+                                                        style={{
+                                                            color:
+                                                                "rgba(0, 0, 0, 0.5)",
+                                                        }}
+                                                    >
+                                                        as
+                                                    </i>{" "}
+                                                    {item.character}
+                                                </Link>
+                                            </li>
+                                        )
+                                )}
+                            </ul>
                         </div>
                     </div>
                 </div>
                 <div className="row mt-3">
                     <div className="col-12">
-                        <h5 className="font-weight-bold pd-2">Tagged Images</h5>
-                        <ListLayout list={image_list.results} />
-                    </div>
-                </div>
-                <div className="row mt-3">
-                    <div className="col-12">
-                        <h5 className="font-weight-bold pd-2">Filmography</h5>
-                        <ListLayout list={test_list} />
+                        <div className="row">
+                            <div
+                                className="col-3 col-sm-2 font-weight-bold media--li"
+                                onClick={() => setStatus(true)}
+                            >
+                                Images {images.length}
+                            </div>
+                            <div
+                                className="col-3 col-sm-2 font-weight-bold media--li"
+                                onClick={() => setStatus(false)}
+                            >
+                                Tagged Images {tagged_images.length}
+                            </div>
+                        </div>
+                        {image_status ? (
+                            // images
+                            <ListLayout type="file" list={images} />
+                        ) : (
+                            // tagged images
+                            <ListLayout type="file" list={tagged_images} />
+                        )}
                     </div>
                 </div>
             </div>
