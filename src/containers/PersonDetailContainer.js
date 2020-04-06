21a1aada5e12110b29d5eeab12d6d11487cc3925
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-06 21:02:26 +0900

    update: redux state 의 데이터 연결

--- src/containers/PersonDetailContainer.js
+++ src/containers/PersonDetailContainer.js
@@ -1,18 +1,33 @@
 import React, { useEffect } from "react";
 import PersonDetail from "../pages/PersonDetail";
-import { people_data } from "../lib/bradpitt";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getPersonDetailThunk } from "../modules/person";
+import stateUtil from "../lib/state_util";
 
 function PersonDetailContainer(props) {
     const dispatch = useDispatch();
     const { match } = props;
-    const { id } = match.params;
+    const id = match.params.id;
+    const { data, loading, error } = useSelector(
+        (state) => state.personReducer[id] || stateUtil.initial()
+    );
+
     useEffect(() => {
-        // dispatch(getDetailContentThunk(id));
-        console.log(people_data.name);
-    }, []);
-    // data
-    return <PersonDetail data={people_data} />;
+        dispatch(getPersonDetailThunk(id));
+    }, [dispatch, id]);
+
+    if (!data) return null;
+    if (loading & !data) return null;
+    if (error) return null;
+
+    return (
+        <PersonDetail
+            detail={data.detail}
+            filmography={data.filmography}
+            images={data.images}
+            tagged_images={data.tagged_images}
+        />
+    );
 }
 
 export default PersonDetailContainer;
