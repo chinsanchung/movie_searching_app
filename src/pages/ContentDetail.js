102d5b25f0d29f7e4aa51c5dbbf1b5828c4e3bb3
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2020-04-08 20:42:20 +0900

    update: 배경 포스터 위치 변경, 점수 추가

--- src/pages/ContentDetail.js
+++ src/pages/ContentDetail.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import ListLayout from "../layout/ListLayout";
-import { runtime_change } from "../lib/functions";
+import { runtime_change } from "../util/functions";
 import { FaRegCalendar } from "react-icons/fa";
 import { IoIosTimer } from "react-icons/io";
+import { FaVoteYea } from "react-icons/fa";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Backdrop = styled.div`
     background-image: url(${(props) => props.url});
@@ -18,7 +20,7 @@ const Backdrop = styled.div`
         left: 0;
         width: 100%;
         height: 100%;
-        background: rgba(0, 0, 0, 0.5);
+        background: rgba(52, 58, 64, 0.3);
     }
 `;
 
@@ -26,85 +28,95 @@ function ContentDetail({ detail, images, credits, videos }) {
     const [media, setMedia] = useState("images");
     useEffect(() => console.log("content"), []);
     return (
-        <div>
+        <>
             <Header />
-            <Backdrop
-                url={`https://image.tmdb.org/t/p/original/${detail.backdrop_path}`}
-                className="container-fluid p-5"
-            >
-                <div className="row" style={{ textIndent: "-9999px" }}>
-                    EMPTY SPACE
-                </div>
-                <div className="row">
-                    <div className="col-12 mb-3">
-                        <h3 className="font-weight-bold">
-                            {detail.original_title}
-                        </h3>
+            <div className="container-fluid pt-5">
+                <Backdrop
+                    url={`https://image.tmdb.org/t/p/original/${detail.backdrop_path}`}
+                >
+                    <div className="row" style={{ textIndent: "-9999px" }}>
+                        EMPTY SPACE
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-12 col-sm-3">
-                        <img
-                            src={`https://image.tmdb.org/t/p/w500/${detail.poster_path}`}
-                            alt={detail.original_title}
-                            className="img-thumbnail content-poster"
-                        />
+                    <div className="row">
+                        <div className="col-12 mb-3">
+                            <h3 className="font-weight-bold">
+                                {detail.original_title}
+                            </h3>
+                        </div>
                     </div>
-                    <div className="col-12 col-sm">
-                        <div>
-                            <p>
-                                {detail.genres.map((genre, index) => (
-                                    <a
-                                        key={index}
-                                        href={`/genre/${genre.id}`}
-                                        className="mr-3 text-white"
-                                    >
-                                        {genre.name}
-                                    </a>
-                                ))}
-                            </p>
-                            <div className="mb-3">
-                                <FaRegCalendar /> {detail.release_date} (US){" "}
-                                <IoIosTimer /> {runtime_change(detail.runtime)}
-                            </div>
-                        </div>
-                        <div>
-                            <h5 className="font-weight-bold">Overview</h5>
-                            <p className="lead">{detail.overview}</p>
-                        </div>
-                        <div>
-                            <h5 className="font-weight-bold">Main Cast</h5>
-                            <ListLayout
-                                type="profile"
-                                list={credits.slice(0, 12)}
+                    <div className="row">
+                        <div className="col-12 col-sm-3">
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500/${detail.poster_path}`}
+                                alt={detail.original_title}
+                                className="img-thumbnail content-poster"
                             />
                         </div>
+                        <div className="col-12 col-sm">
+                            <div>
+                                <p>
+                                    {detail.genres.map((genre, index) => (
+                                        <a
+                                            key={index}
+                                            href={`/genre/${genre.id}`}
+                                            className="mr-3 text-white"
+                                        >
+                                            {genre.name}
+                                        </a>
+                                    ))}
+                                </p>
+                                <div className="mb-3">
+                                    <span className="mr-3">
+                                        <FaRegCalendar /> {detail.release_date}{" "}
+                                        (US)
+                                    </span>
+                                    <span className="mr-3">
+                                        <IoIosTimer />{" "}
+                                        {runtime_change(detail.runtime)}
+                                    </span>
+                                    <span className="mr-3">
+                                        <FaVoteYea /> {detail.vote_average} / 10
+                                    </span>
+                                </div>
+                            </div>
+                            <div>
+                                <h5 className="font-weight-bold">Overview</h5>
+                                <p className="lead">{detail.overview}</p>
+                            </div>
+                            <div>
+                                <h5 className="font-weight-bold">Main Cast</h5>
+                                <ListLayout
+                                    type="profile"
+                                    list={credits.slice(0, 12)}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="row mt-3">
+                </Backdrop>
+                <div className="row mt-3 ml-1">
                     <div className="col-12">
                         <div className="row">
-                            <div className="col-1 mr-3">
-                                <h4>Media</h4>
-                            </div>
-                            <div
-                                className="col-2 font-weight-bold media--li"
+                            <span className="font-weight-bold media--title">
+                                Media
+                            </span>
+                            <span
+                                className="font-weight-bold media--li mr-2"
                                 onClick={() => setMedia("images")}
                             >
                                 Images {images.backdrops.length}
-                            </div>
-                            <div
-                                className="col-2 font-weight-bold media--li"
+                            </span>
+                            <span
+                                className="font-weight-bold media--li mr-2"
                                 onClick={() => setMedia("posters")}
                             >
                                 Posters {images.posters.length}
-                            </div>
-                            <div
-                                className="col-2 font-weight-bold media--li"
+                            </span>
+                            <span
+                                className="font-weight-bold media--li"
                                 onClick={() => setMedia("videos")}
                             >
                                 Videos {videos.length}
-                            </div>
+                            </span>
                         </div>
                         {(() => {
                             switch (media) {
@@ -133,16 +145,10 @@ function ContentDetail({ detail, images, credits, videos }) {
                                     return null;
                             }
                         })()}
-                        <div className="row mt-3">
-                            <h5 className="font-weight-bold">
-                                Recommandations
-                            </h5>
-                            {/* <ListLayout type="poster" list={recommandations} /> */}
-                        </div>
                     </div>
                 </div>
-            </Backdrop>
-        </div>
+            </div>
+        </>
     );
 }
 
